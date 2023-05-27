bd88049eac22925bc17353e7b309c863ba847dda
Symplice BONI <bonisimplice@gmail.com>
2023-05-27 03:12:49 +0000

    Gather data from an API

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -3,21 +3,33 @@
 Python script that, using this REST API, for a given employee ID,
 returns information about his/her TODO list progress.
 """
+from json import load
 import requests
-import sys
+from sys import argv
 
 if __name__ == "__main__":
-    output = "Employee {EMPLOYEE_NAME} is done " \
-             "with tasks({NUMBER_OF_DONE_TASKS}/{TOTAL_NUMBER_OF_TASKS}):"
-    employee_id = sys.argv[1]
-    url = "https://jsonplaceholder.typicode.com/"
-    employee = requests.get(url + "users/{}".format(employee_id)).json()
-    todos = requests.get(url + "todos", params={"userId": employee_id}).json()
 
-    done_tasks = [t.get("title") for t in todos if t.get("completed", False)]
-    print(output.format(
-        EMPLOYEE_NAME=employee.get("name"),
-        NUMBER_OF_DONE_TASKS=len(done_tasks),
-        TOTAL_NUMBER_OF_TASKS=len(todos)))
-    for c in done_tasks:
-        print("\t {}".format(c))
+    def make_request(resource, param=None):
+        """Retrieve user from API
+        """
+        url = 'https://jsonplaceholder.typicode.com/'
+        url += resource
+        if param:
+            url += ('?' + param[0] + '=' + param[1])
+
+        # make request
+        r = requests.get(url)
+
+        # extract json response
+        r = r.json()
+        return r
+
+    user = make_request('users', ('id', argv[1]))
+    tasks = make_request('todos', ('userId', argv[1]))
+    tasks_completed = [task for task in tasks if task['completed']]
+
+    print('Employee {} is done with tasks({}/{}):'.format(user[0]['name'],
+                                                          len(tasks_completed),
+                                                          len(tasks)))
+    for task in tasks_completed:
+        print('\t {}'.format(task['title']))
